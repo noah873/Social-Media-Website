47f8dee3af67bb10ecf410b1d5b24b875e86ff15
noah873 <157926076+noah873@users.noreply.github.com>
2024-08-28 23:58:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js';
+import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, query, orderBy, limit, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js';
 
 const firebaseConfig = {
