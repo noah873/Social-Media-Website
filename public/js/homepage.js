21ed51b08d3900d945f06cea98abae294867b933
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-08 17:23:46 -0700

    Create homepage.js

---
+++ public/js/homepage.js
@@ -0,0 +1,39 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCCK38R9btLtT5SLCwRB97vv9qbj8RFM",
+  authDomain: "social-media-website-db1fe.firebaseapp.com",
+  projectId: "social-media-website-db1fe",
+  storageBucket: "social-media-website-db1fe.appspot.com",
+  messagingSenderId: "989794613612",
+  appId: "1:989794613612:web:b757ace6b828d44087db49"
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const signOutButton = document.getElementById('sign-out');
+
+function updateUI(user) {
+    if (user) {
+        loginDiv.classList.add('hidden');
+        homepageDiv.classList.remove('hidden');
+    } else {
+        loginDiv.classList.remove('hidden');
+        homepageDiv.classList.add('hidden');
+    }
+}
+
+onAuthStateChanged(auth, user => {
+    updateUI(user);
+});
+
+signOutButton.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        updateUI(null);
+        emailInput.value = '';
+        passwordInput.value = '';
+        messageDiv.textContent = '';
+    });
+});
