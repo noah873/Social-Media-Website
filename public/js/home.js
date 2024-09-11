909fe220615e9481cea86c1691be190b2714d1de
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:32:33 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,9 @@
+function setupHomeElements() {
+  const signOutButton = document.getElementById('signOut');
+  
+  signOutButton.addEventListener('click', () => {
+    signOut(auth)
+  });
+}
 
+export {setupHomeElements};
