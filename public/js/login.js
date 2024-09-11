7113c278706241f8166791f8b497663349382593
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:40:01 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCCK38R9btLtT5SLCwRB97vv9qbj8RFM",
+  authDomain: "social-media-website-db1fe.firebaseapp.com",
+  projectId: "social-media-website-db1fe",
+  storageBucket: "social-media-website-db1fe.appspot.com",
+  messagingSenderId: "989794613612",
+  appId: "1:989794613612:web:b757ace6b828d44087db49"
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
 function setupLoginElements() {
   const messageDiv = document.getElementById('message');
   const emailInput = document.getElementById('email');
