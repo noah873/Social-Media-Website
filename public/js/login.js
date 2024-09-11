4ff4106e4cbf8ba4dcfb9085654dfb5173f1533e
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:31:58 -0700

    Create login.js

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+function setupLoginElements() {
+  const messageDiv = document.getElementById('message');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  
+  const signInButton = document.getElementById('signIn');
+  const createAccountButton = document.getElementById('createAccount');
+
+  signInButton.addEventListener('click', () => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .catch(() => {
+            messageDiv.textContent = 'Invalid Email or Password';
+        });
+  });
+
+  createAccountButton.addEventListener('click', () => {
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .catch(() => {
+            messageDiv.textContent = 'Error Creating Account';
+        });
+  });
+}
+
+export {setupLoginElements};
