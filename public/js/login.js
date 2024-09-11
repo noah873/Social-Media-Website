0d454b45d8d3d7c6db4293447a6386da08d66572
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:51:23 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-import { app, auth } from 'firebase.js';
+import { app, auth } from './firebase.js';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
 
 function setupLoginElements() {
