2b2ca76b6ec82cca95228968d5f09cb3c71eaa9f
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:50:12 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,17 +1,5 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
-import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCaCCK38R9btLtT5SLCwRB97vv9qbj8RFM",
-  authDomain: "social-media-website-db1fe.firebaseapp.com",
-  projectId: "social-media-website-db1fe",
-  storageBucket: "social-media-website-db1fe.appspot.com",
-  messagingSenderId: "989794613612",
-  appId: "1:989794613612:web:b757ace6b828d44087db49"
-};
-
-initializeApp(firebaseConfig);
-const auth = getAuth();
+import { app, auth } from 'firebase.js';
+import { signOut } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
 
 function setupHomeElements() {
   const signOutButton = document.getElementById('signOut');
