a0117704db2b6d5e93ee6f3312c12d9416c91984
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-11 16:52:18 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,20 +1,9 @@
+import { app, auth } from '.js/firebase.js'
+
 import {setupLoginElements} from './js/login.js';
 import {setupHomeElements} from './js/home.js';
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCaCCK38R9btLtT5SLCwRB97vv9qbj8RFM",
-  authDomain: "social-media-website-db1fe.firebaseapp.com",
-  projectId: "social-media-website-db1fe",
-  storageBucket: "social-media-website-db1fe.appspot.com",
-  messagingSenderId: "989794613612",
-  appId: "1:989794613612:web:b757ace6b828d44087db49"
-};
-
-initializeApp(firebaseConfig);
-const auth = getAuth();
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
 
 async function loadHTML(html) {
   const response = await fetch(`html/${html}`);
