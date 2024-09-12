1e7212be409e492f4a6bcb082441c9508855b205
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-12 12:40:50 -0700

    Update createAccount.js

--- public/html/createAccount.js
+++ public/html/createAccount.js
@@ -1,1 +1,21 @@
+function setupCreateAccountElements() {
+  const messageDiv = document.getElementById('message');
 
+  const  = document.getElementById('');
+  const  = document.getElementById('');
+  
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  
+  const createAccountButton = document.getElementById('createAccount');
+  
+  createAccountButton.addEventListener('click', () => {
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .catch(() => {
+            messageDiv.textContent = 'Error Creating Account';
+        });
+  });
+}
