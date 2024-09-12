eaefe9ee1646b6453c6597e113f9e86851dd5d69
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-12 12:50:27 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 import { app, auth } from './firebase.js';
 import { renderHTML } from '../app.js';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
+import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
 
 function setupLoginElements() {
   const messageDiv = document.getElementById('message');
