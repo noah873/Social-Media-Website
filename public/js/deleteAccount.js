1fefd29bfbf837e481c40b6f8cd02848d139c91f
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-14 14:36:46 -0700

    Create deleteAccount.js

---
+++ public/js/deleteAccount.js
@@ -0,0 +1,15 @@
+import { renderHTML } from '../app.js';
+
+function setupDeleteAccountElements() {
+  const messageDiv = document.getElementById('message');
+  
+  const createAccountButton = document.getElementById('createAccount');
+  const deleteAccountButton = document.getElementById('deleteAccountButton');
+  const settingsButton = document.getElementById('settings');
+
+  settingsButton.addEventListener('click', () => {
+    renderHTML("settings.html");
+  });
+}
+
+export { setupDeleteAccountElements };
