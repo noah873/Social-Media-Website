76fe6003ae01b08042f72819fdcae20ee0386ea0
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-14 15:17:17 -0700

    Update deleteAccount.js

--- public/js/deleteAccount.js
+++ public/js/deleteAccount.js
@@ -1,13 +1,32 @@
-import { doc, deleteDoc } from './firebase.js'
+import { auth, deleteUser, doc, deleteDoc } from './firebase.js'
 import { renderHTML } from '../app.js';
 
 function setupDeleteAccountElements() {
   const messageDiv = document.getElementById('message');
+  const message2Div = document.getElementById('message2');
   
-  const createAccountButton = document.getElementById('createAccount');
   const deleteAccountButton = document.getElementById('deleteAccountButton');
   const settingsButton = document.getElementById('settings');
 
+  deleteAccountButton.addEventListener('click', () => {
+    const user = userCredential.user;
+    await deleteDoc(doc(db, 'users', user.uid);
+
+    messageDiv.textContent = 'Data Deletion Successful';
+    message2Div.textContent = 'Attempting to Delete Account now, you will automatically be redirectly to the login page if successful.';
+
+    setTimeout(function() {
+      const currentUser = auth.currentUser;
+      deleteUser(currentUser)
+        .catch((error) => {
+          console.error('Error Deleting Account', error);
+          messageDiv.textContent = 'Error Deleting Account';
+          message2Div.textContent = '';
+        });
+    }, 5000);
+    
+  });
+  
   settingsButton.addEventListener('click', () => {
     renderHTML("settings.html");
   });
