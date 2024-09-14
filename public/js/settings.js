5f037ed6650d2b5ea3302e03511b4d3f178f1b1b
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-14 14:39:05 -0700

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -4,6 +4,7 @@ import { renderHTML } from '../app.js';
 function setupSettingsElements() {
   const homeButton = document.getElementById('home');
   const signOutButton = document.getElementById('signOut');
+  const deleteAccountButton = document.getElementById('deleteAccount');
 
   homeButton.addEventListener('click', () => {
     renderHTML("home.html");
@@ -11,6 +12,10 @@ function setupSettingsElements() {
   
   signOutButton.addEventListener('click', () => {
     signOut(auth)
+  });
+
+  deleteAccountButton.addEventListener('click', () => {
+    renderHTML("deleteAccount.html");
   });
 }
 
