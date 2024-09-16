a50011ec52ae30a57559a4f08d79057fd66cbc83
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-16 15:30:03 -0700

    Update changePassword.js

--- public/js/changePassword.js
+++ public/js/changePassword.js
@@ -1,7 +1,58 @@
+import { auth, reauthenticateWithCredential, EmailAuthProvider, updatePassword } from './firebase.js'
 import { renderHTML } from '../app.js';
 
 function setupChangePasswordElements() {
+  const messageDiv = document.getElementById('message');
+  const passwordInput = document.getElementById('password');
+  const newPasswordInput = document.getElementById('newPassword');
+  const confirmNewPasswordInput = document.getElementById('confirmNewPassword');
+  const changePasswordButton = document.getElementById('changePassword');
+  
   const settingsButton = document.getElementById('settings');
+  
+  changePasswordButton.addEventListener('click', async () => {
+    const password = passwordInput.value;
+    const newPassword = newPasswordInput.value;
+    const confirmNewPassword = confirmNewPasswordInput.value;
+
+    if (newPassword !== confirmNewPassword) {
+        messageDiv.textContent = 'New Passwords do not Match';
+        return;
+    }
+
+    const user = auth.currentUser;
+
+    try {
+        const credential = EmailAuthProvider.credential(user.email, password);
+        await reauthenticateWithCredential(user, credential);
+        await updatePassword(user, newPassword);
+        messageDiv.textContent = 'Password Updated Successfully';
+        password = '';
+        newPassword = '';
+        confirmNewPassword = '';
+    } catch (error) {
+        console.error(error);
+        messageDiv.textContent = 'Error Changing Password';
+    }
+  });
+  
+  passwordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      settingsButton.click();
+    }
+  });
+
+  newPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      settingsButton.click();
+    }
+  });
+
+  confirmNewPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      settingsButton.click();
+    }
+  });
 
   settingsButton.addEventListener('click', () => {
     renderHTML("settings.html");
