29a72e98db12104482deeb81b5a96e42b3c8b922
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-20 17:03:43 -0700

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,16 +1,39 @@
-import { auth, signOut } from './firebase.js';
+import { auth, signOut, doc, onSnapshot } from './firebase.js';
 import { renderHTML } from '../app.js';
 
 function setupSettingsElements() {
   const homeButton = document.getElementById('home');
+
+  const messageDiv = document.getElementById('message');
+  const fullNameInput = document.getElementById('fullName');
+  const changeFullNameButton = document.getElementById('changeFullName');
+
+  const message2Div = document.getElementById('message2');
+  const usernameInput = document.getElementById('username');
+  const changeUsernameButton = document.getElementById('changeUsername');
+  
+  const changeEmailButton = document.getElementById('changeEmail');
   const changePasswordButton = document.getElementById('changePassword');
   const signOutButton = document.getElementById('signOut');
   const deleteAccountButton = document.getElementById('deleteAccount');
 
+  const user = auth.currentUser;
+  const userRef = doc(db, 'users', user.uid);
+  
   homeButton.addEventListener('click', () => {
     renderHTML("home.html");
   });
 
+  onSnapshot(userRef, (doc) => {
+    const data = doc.data();
+    messageDiv.textContent = 'Full Name: ' + data.fullName;
+  });
+
+  onSnapshot(userRef, (doc) => {
+    const data = doc.data();
+    message2Div.textContent = 'Username: ' + data.username;
+  });
+  
   changePasswordButton.addEventListener('click', () => {
     renderHTML("changePassword.html");
   });
