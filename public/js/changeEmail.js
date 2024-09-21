e6cb7e0bb9a17569c0f4b17dac0f8d5f19338e61
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-20 19:23:09 -0700

    Update changeEmail.js

--- public/js/changeEmail.js
+++ public/js/changeEmail.js
@@ -20,10 +20,13 @@ function setupChangeEmailElements() {
         .then(() => {
           updateEmail(user, newEmail)
             .then(() => {
-              messageDiv.textContent = 'Email Change Successful';
-              message2Div.textContent = 'An email will be sent to your old email in case this was a mistake.';
-              passwordInput.value = '';
-              newEmailInput.value = '';
+              sendEmailVerification(user)
+                .then(() => {
+                  messageDiv.textContent = 'Email Change Successful';
+                  message2Div.textContent = 'An email will be sent to your old email in case this was a mistake.';
+                  passwordInput.value = '';
+                  newEmailInput.value = '';
+                })
             })
             .catch((error) => {
                 console.error('Error Changing Email:', error);
