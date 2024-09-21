0dd06431cd4cb7a29359ac008c48d49b2d0d7fb4
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-20 18:54:14 -0700

    Update changeEmail.js

--- public/js/changeEmail.js
+++ public/js/changeEmail.js
@@ -1,1 +1,17 @@
+import { auth, reauthenticateWithCredential, EmailAuthProvider, updateEmail } from './firebase.js'
+import { renderHTML } from '../app.js';
 
+function setupChangeEmailElements() {
+  const messageDiv = document.getElementById('message');
+  const message2Div = document.getElementById('message2');
+  const passwordInput = document.getElementById('password');
+  const newEmailInput = document.getElementById('newEmail');
+  const changeEmailButton = document.getElementById('changeEmailButton');
+  const settingsButton = document.getElementById('settings');
+
+  settingsButton.addEventListener('click', () => {
+    renderHTML("settings.html");
+  });
+}
+
+export { setupChangeEmailElements };
