e82c3386731ed5e487c05ded77c830a9bfbd846c
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-24 15:42:13 -0700

    Update createAccount.js

--- public/js/createAccount.js
+++ public/js/createAccount.js
@@ -18,60 +18,62 @@ function setupCreateAccountElements() {
     const username = usernameInput.value;
     const email = emailInput.value;
     const password = passwordInput.value;
-
-    const currentUsers = return getDocs(collection(db, 'users'));
-    const takenUsernames = currentUsers.docs.map(doc => doc.data().username);
-
-    if (takenUsernames.includes(username)) {
-      messageDiv.textContent = 'Username Already Taken';
-      return;
-    }
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        const userRef = doc(db, 'users', user.uid);
-        
-        return setDoc(userRef, {
-          email: email,
-          full_name: fullName,
-          username: username,
-          online_status: true
-        });
+  
+    getDocs(collection(db, 'users'))
+      .then(currentUsers => {
+        const takenUsernames = currentUsers.docs.map(doc => doc.data().username);
+  
+        if (takenUsernames.includes(username)) {
+          messageDiv.textContent = 'Username Already Taken';
+          return;
+        }
+  
+        return createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            const userRef = doc(db, 'users', user.uid);
+            
+            return setDoc(userRef, {
+              email: email,
+              full_name: fullName,
+              username: username,
+              online_status: true
+            });
+          });
       })
       .catch((error) => {
-        console.error('Error Creating Account and/or Writing Data: ', error);
+        console.error('Error Creating Account: ', error);
         messageDiv.textContent = 'Error Creating Account';
       });
-    });
+  });
 
-    fullNameInput.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        createAccountButton.click();
-      }
-    });
+  fullNameInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      createAccountButton.click();
+    }
+  });
 
-    usernameInput.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        createAccountButton.click();
-      }
-    });
+  usernameInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      createAccountButton.click();
+    }
+  });
 
-    emailInput.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        createAccountButton.click();
-      }
-    });
+  emailInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      createAccountButton.click();
+    }
+  });
 
-    passwordInput.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        createAccountButton.click();
-      }
-    });
+  passwordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      createAccountButton.click();
+    }
+  });
     
-    loginButton.addEventListener('click', () => {
-      renderHTML("login.html");
-    });
+  loginButton.addEventListener('click', () => {
+    renderHTML("login.html");
+  });
 }
 
 export { setupCreateAccountElements };
