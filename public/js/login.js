96cfce749b778892583fee45459c5dc6d9077be0
noah873 <157926076+noah873@users.noreply.github.com>
2024-09-26 11:59:12 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,8 @@ import { renderHTML } from '../app.js';
 
 function setupLoginElements() {
   const messageDiv = document.getElementById('message');
+  const message2Div = document.getElementById('message2');
+  
   const emailInput = document.getElementById('email');
   const passwordInput = document.getElementById('password');
   
@@ -14,10 +16,27 @@ function setupLoginElements() {
     const email = emailInput.value;
     const password = passwordInput.value;
 
+    if (email === '') {
+      message2Div.textContent = 'Please enter your email.';
+      return;
+    }
+
+    if (password === '') {
+      message2Div.textContent = 'Please enter your password.';
+      return;
+    }
+
     signInWithEmailAndPassword(auth, email, password)
         .catch((error) => {
             console.error('Error Signing In: ', error);
-            messageDiv.textContent = 'Invalid Email or Password';
+            messageDiv.textContent = 'Error Signing In';
+
+            let errorMessage = error.message; // sample input: " Firebase: Password should be at least 6 characters (auth/weak-password)."
+            errorMessage = errorMessage.trim().split(" "); // removes excess spaces and splits into array
+            errorMessage[errorMessage.length - 1] = errorMessage[errorMessage.length - 1].replace("auth/", ""); // changes last element "(auth/weak-password)." to "(weak-password)."
+            errorMessage = errorMessage.slice(1, errorMessage.length); // slices off first element in array
+            errorMessage = errorMessage.join(" ") + " Please try again."; // rejoins array into string
+            message2Div.textContent = errorMessage;
         });
   });
 
