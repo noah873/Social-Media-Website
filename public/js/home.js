9601ddbbc9a35b4937b23e09cbe1e14ffae5a2bd
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-01 11:27:11 -0700

    Delete public directory
    I cant revert to a previous version, so I'm deleting public and uploading my backup (1/2)

--- public/js/home.js
+++
@@ -1,7 +0,0 @@
-import { renderHTML } from '../app.js';
-
-const settingsButton = document.getElementById('settings');
-
-settingsButton.addEventListener('click', () => {
-  renderHTML("settings.html");
-});
