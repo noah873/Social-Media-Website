39038be8da9882cc81bb073e20a5badabf7a4422
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-11 13:41:51 -0700

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,7 +4,7 @@ function setupNavbarElements(activePage) {
   const buttons = {
     home: { button: document.getElementById('navHome'), page: "home.html" },
     messages: { button: document.getElementById('navMessages'), page: "messages.html" },
-    post: { button: document.getElementById('navCreatePost'), page: "createPost.html" },
+    createPost: { button: document.getElementById('navCreatePost'), page: "createPost.html" },
     settings: { button: document.getElementById('navSettings'), page: "settings.html" }
   };
 
