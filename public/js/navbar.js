ca663e5f069050a9fa2ff0c5ef1a0b9c21848536
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-11 12:30:20 -0700

    Create navbar.js

---
+++ public/js/navbar.js
@@ -0,0 +1,26 @@
+import { renderHTML } from '../app.js';
+
+function setupNavbarElements() {
+  const navHomeButton = document.getElementById('navHome');
+  const navMessagesButton = document.getElementById('navMessages');
+  const navPostButton = document.getElementById('navPost');
+  const navSettingsButton = document.getElementById('navSettings');
+  
+  navHomeButton.addEventListener('click', () => {
+    renderHTML("home.html");
+  });
+  
+  navMessagesButton.addEventListener('click', () => {
+    renderHTML("messages.html");
+  });
+  
+  navPostButton.addEventListener('click', () => {
+    renderHTML("home.html");
+  });
+  
+  navSettingsButton.addEventListener('click', () => {
+    renderHTML("settings.html");
+  });
+}
+
+export { setupNavbarElements };
