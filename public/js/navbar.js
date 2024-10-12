9e5bee7c4e33d48ef8e9c50d195d0d700d1bc2cd
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-11 20:51:19 -0700

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,13 +5,13 @@ function setupNavbarElements(activePage) {
     home: { button: document.getElementById('navHome'), page: "home.html" },
     messages: { button: document.getElementById('navMessages'), page: "messages.html" },
     createPost: { button: document.getElementById('navCreatePost'), page: "createPost.html" },
-    settings: { button: document.getElementById('navSettings'), page: "settings.html" },
-    profile: { button: document.getElementById('navProfile'), page: "profile.html" } 
+    profile: { button: document.getElementById('navProfile'), page: "profile.html" } ,
+    settings: { button: document.getElementById('navSettings'), page: "settings.html" }
   };
 
-  buttons[activePage].button.style.backgroundColor = '#0056b3';
-  buttons[activePage].button.style.cursor = 'default';
-  delete buttons[activePage];
+  buttons[activePage].button.style.backgroundColor = '#0056b3'; // set the color of the active page button to look like its depressed (darker blue)
+  buttons[activePage].button.style.cursor = 'default'; // remove mouseover selection visual
+  delete buttons[activePage]; // to prevent the creation of an event listener to redirect to that page as the user is already on it
   
   Object.keys(buttons).forEach(buttonID => {
     const { button, page } = buttons[buttonID];
