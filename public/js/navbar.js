31f74a81ce244414e929217b327aef3d346410b2
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-11 17:07:42 -0700

    Update navbar.js
    added a comma

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,7 +5,7 @@ function setupNavbarElements(activePage) {
     home: { button: document.getElementById('navHome'), page: "home.html" },
     messages: { button: document.getElementById('navMessages'), page: "messages.html" },
     createPost: { button: document.getElementById('navCreatePost'), page: "createPost.html" },
-    settings: { button: document.getElementById('navSettings'), page: "settings.html" }
+    settings: { button: document.getElementById('navSettings'), page: "settings.html" },
     profile: { button: document.getElementById('navProfileButton'), page: 'profile.html' } 
   };
 
