1ed8c930146d1d8798c84b0b344eb594dbe1955a
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-13 18:21:19 -0700

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-import { auth, signOut, db, doc, onSnapshot, updateDoc, collection, getDocs } from './firebase.js';
+import { auth, signOut, reauthenticateWithCredential, EmailAuthProvider, updateEmail, updatePassword, deleteUser, db, doc, onSnapshot, updateDoc, collection, getDocs, deleteDoc } from './firebase.js';
 import { renderHTML } from '../app.js';
 
 function setupSettingsElements() {
@@ -107,11 +107,11 @@ function setupSettingsElements() {
   });
 
   settingsChangeEmailButton.addEventListener('click', () => {
-    renderHTML("changeEmail.html");
+    changeEmailPopup.classList.remove('hidden');
   });
 
   settingsChangePasswordButton.addEventListener('click', () => {
-    renderHTML("changePassword.html");
+    changePasswordPopup.classList.remove('hidden');
   });
   
   settingsSignOutButton.addEventListener('click', () => {
@@ -119,7 +119,224 @@ function setupSettingsElements() {
   });
 
   settingsDeleteAccountButton.addEventListener('click', () => {
-    renderHTML("deleteAccount.html");
+    deleteAccountPopup.classList.remove('hidden');
+  });
+
+  // Popup Functionalities
+  const changeEmailPopup = document.getElementById('changeEmailPopup');
+  const changePasswordPopup = document.getElementById('changePasswordPopup');
+  const deleteAccountPopup = document.getElementById('deleteAccountPopup');
+
+  changeEmailPopup.addEventListener('click', function() {
+    if (event.target === this) {
+      changeEmailPopup.classList.add('hidden');
+    }
+  });
+
+  changePasswordPopup.addEventListener('click', function() {
+    if (event.target === this) {
+      changePasswordPopup.classList.add('hidden');
+    }
+  });
+
+  deleteAccountPopup.addEventListener('click', deleteAccountPopupClick);
+
+  function deleteAccountPopupClick() {
+    if (event.target === this) {
+        deleteAccountPopup.classList.add('hidden');
+    }
+  }
+
+  // Change Email Popup
+  const changeEmailMessageDiv = document.getElementById('changeEmailMessage');
+  const changeEmailMessage2Div = document.getElementById('changeEmailMessage2');
+  const changeEmailPasswordInput = document.getElementById('changeEmailPassword');
+  const changeEmailNewEmailInput = document.getElementById('changeEmailNewEmail');
+  const changeEmailChangeEmailButton = document.getElementById('changeEmailChangeEmailButton');
+  const changeEmailSettingsButton = document.getElementById('changeEmailSettings');
+
+  changeEmailChangeEmailButton.addEventListener('click', () => {
+    const password = changeEmailPasswordInput.value;
+    const newEmail = changeEmailNewEmailInput.value;
+    
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, password);
+    const userRef = doc(db, 'users', user.uid);
+
+    reauthenticateWithCredential(user, credential)
+        .then(() => {
+          if (newEmail === '') {
+            changeEmailMessageDiv.textContent = 'Please Enter a New Email';
+            return;
+          }
+          
+          updateEmail(user, newEmail)
+            .then(() => {
+              changeEmailMessageDiv.textContent = 'Email Change Successful';
+              changeEmailMessage2Div.textContent = 'An email will be sent to your old email address in case this was a mistake.';
+              changeEmailPasswordInput.classList.add('hidden');
+              changeEmailNewEmailInput.classList.add('hidden');
+              changeEmailChangeEmailButton.classList.add('hidden');
+
+              return updateDoc(userRef, {
+                email: newEmail
+              })
+                .catch(error => {
+                  console.error(error);
+                });
+            })
+            .catch((error) => {
+              console.error('Error Changing Email:', error);
+              changeEmailMessageDiv.textContent = 'Error Changing Email';
+            });
+        })
+        .catch((error) => {
+            console.error('Error during Reauthentication:', error);
+            changeEmailMessageDiv.textContent = 'Error during Reauthentication';
+        });
+  });
+
+  changeEmailPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      changeEmailChangeEmailButton.click();
+    }
+  });
+
+  changeEmailNewEmailInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      changeEmailChangeEmailButton.click();
+    }
+  });
+  
+  changeEmailSettingsButton.addEventListener('click', () => {
+    changeEmailPopup.classList.add('hidden');
+  });
+
+  // Change Password Popup
+  const changePasswordMessageDiv = document.getElementById('changePasswordMessage');
+  const changePasswordPasswordInput = document.getElementById('changePasswordPassword');
+  const changePasswordNewPasswordInput = document.getElementById('changePasswordNewPassword');
+  const changePasswordConfirmNewPasswordInput = document.getElementById('changePasswordConfirmNewPassword');
+  const changePasswordChangePasswordButton = document.getElementById('changePasswordChangePasswordButton');
+  
+  const changePasswordSettingsButton = document.getElementById('changePasswordSettings');
+  
+  changePasswordChangePasswordButton.addEventListener('click', () => {
+    const password = changePasswordPasswordInput.value;
+    const newPassword = changePasswordNewPasswordInput.value;
+    const confirmNewPassword = changePasswordConfirmNewPasswordInput.value;
+    
+    if (newPassword !== confirmNewPassword) {
+        changePasswordMessageDiv.textContent = 'New Passwords do not Match';
+        return;
+    }
+
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, password);
+
+    reauthenticateWithCredential(user, credential)
+        .then(() => {
+          if (newPassword === '') {
+            changePasswordMessageDiv.textContent = 'Please Fill Out all Forms';
+            return;
+          }
+          
+          updatePassword(user, newPassword)
+            .then(() => {
+              changePasswordMessageDiv.textContent = 'Password Updated Successfully';
+              
+              changePasswordPasswordInput.classList.add('hidden');
+              changePasswordNewPasswordInput.classList.add('hidden');
+              changePasswordConfirmNewPasswordInput.classList.add('hidden');
+              changePasswordChangePasswordButton.classList.add('hidden');
+            })
+            .catch((error) => {
+              console.error('Error Changing Password:', error);
+              changePasswordMessageDiv.textContent = 'Error Changing Password';
+            });
+        })
+        .catch((error) => {
+            console.error('Error during Reauthentication:', error);
+            changePasswordMessageDiv.textContent = 'Error during Reauthentication';
+        });
+  });
+
+  changePasswordPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      changePasswordChangePasswordButton.click();
+    }
+  });
+
+  changePasswordNewPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      changePasswordChangePasswordButton.click();
+    }
+  });
+
+  changePasswordConfirmNewPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      changePasswordChangePasswordButton.click();
+    }
+  });
+
+  changePasswordSettingsButton.addEventListener('click', () => {
+    changePasswordPopup.classList.add('hidden');
+  });
+
+  // Delete Account Popup
+  const deleteAccountDiv = document.getElementById('deleteAccount');
+  
+  const deleteAccountMessageDiv = document.getElementById('deleteAccountMessage');
+  const deleteAccountMessage2Div = document.getElementById('deleteAccountMessage2');
+
+  const deleteAccountPasswordInput = document.getElementById('deleteAccountPassword');
+  const deleteAccountDeleteAccountButton = document.getElementById('deleteAccountDeleteAccountButton');
+  const deleteAccountSettingsButton = document.getElementById('deleteAccountSettings');
+
+  deleteAccountDeleteAccountButton.addEventListener('click', async () => {
+    const password = deleteAccountPasswordInput.value;
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, password);
+
+    reauthenticateWithCredential(user, credential)
+        .then(() => {
+          return deleteDoc(doc(db, 'users', user.uid));
+        })
+        .then(() => {
+          const currentUser = auth.currentUser;
+          // serves as a flag to prevent redirection to login page
+          sessionStorage.setItem('deletingAccount', 'true');
+          deleteUser(currentUser)
+            .then(() => {
+              deleteAccountPopup.style.backgroundColor = 'white';
+              deleteAccountPopup.removeEventListener('click', deleteAccountPopupClick);
+              deleteAccountDiv.style.width = '25%';
+              deleteAccountMessageDiv.textContent = 'Account and Data Deletion Successful';
+              deleteAccountMessage2Div.textContent = 'Refresh the page to return to login screen.';
+              deleteAccountPasswordInput.classList.add('hidden');
+              deleteAccountDeleteAccountButton.classList.add('hidden');
+              deleteAccountSettingsButton.classList.add('hidden');
+            })
+            .catch((error) => {
+              console.error('Error Deleting Account', error);
+              deleteAccountMessageDiv.textContent = 'Error Deleting Account';
+              deleteAccountMessage2Div.textContent = '';
+            });
+        })
+        .catch((error) => {
+            console.error('Error during Reauthentication:', error);
+            deleteAccountMessageDiv.textContent = 'Error during Reauthentication';
+        });
+  });
+
+  deleteAccountPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      deleteAccountDeleteAccountButton.click();
+    }
+  });
+  
+  deleteAccountSettingsButton.addEventListener('click', () => {
+    deleteAccountPopup.classList.add('hidden');
   });
 }
 
