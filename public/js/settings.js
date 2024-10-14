0a3cd5ffc7fe91ed83499e76c9ee6da3e3c83f89
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-13 18:23:14 -0700

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,5 +1,4 @@
 import { auth, signOut, reauthenticateWithCredential, EmailAuthProvider, updateEmail, updatePassword, deleteUser, db, doc, onSnapshot, updateDoc, collection, getDocs, deleteDoc } from './firebase.js';
-import { renderHTML } from '../app.js';
 
 function setupSettingsElements() {
   const settingsMessageDiv = document.getElementById('settingsMessage');
