7043c4bdabef3e93eb0e3cdde77cdc2c44c82f53
sionfallah <116696121+sionfallah@users.noreply.github.com>
2024-10-20 22:26:48 -0700

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,6 +4,7 @@ function setupNavbarElements(activePage) {
   const buttons = {
     home: { button: document.getElementById('navHome'), page: "home.html" },
     messages: { button: document.getElementById('navMessages'), page: "messages.html" },
+    friends: { button: document.getElementById('navFriends'), page: "friends.html" },
     createPost: { button: document.getElementById('navCreatePost'), page: "createPost.html" },
     profile: { button: document.getElementById('navProfile'), page: "profile.html" } ,
     settings: { button: document.getElementById('navSettings'), page: "settings.html" }
