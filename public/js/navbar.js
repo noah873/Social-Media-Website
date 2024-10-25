21cfc31b2762d462ab6cf1c983bc63e2c6ab95b9
noah873 <157926076+noah873@users.noreply.github.com>
2024-10-25 11:13:22 -0700

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -9,9 +9,13 @@ function setupNavbarElements(activePage) {
     profile: { button: document.getElementById('navProfile'), page: "profile.html" } ,
     settings: { button: document.getElementById('navSettings'), page: "settings.html" }
   };
-
-  buttons[activePage].button.style.backgroundColor = '#0056b3'; // set the color of the active page button to look like its depressed (darker blue)
-  buttons[activePage].button.style.cursor = 'default'; // remove mouseover selection visual
+  
+  const activeButton = buttons[activePage].button;
+  const clonedButton = activeButton.cloneNode(true);
+  activeButton.parentNode.replaceChild(clonedButton, activeButton);
+  
+  activeButton.style.backgroundColor = '#0056b3'; // set the color of the active page button to look like its depressed (darker blue)
+  activeButton.style.cursor = 'default'; // remove mouseover selection visual
   delete buttons[activePage]; // to prevent the creation of an event listener to redirect to that page as the user is already on it
   
   Object.keys(buttons).forEach(buttonID => {
